5a0ca563be0e3323a4df94b00ac675658d9887c2
Woozyzzz <woozyzzz@foxmail.com>
2024-04-10 19:24:55 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import ElementUI from "element-ui";
+import "./assets/styles/element-variables.scss";
+import "./assets/styles/reset.scss";
+import "./assets/styles/help.scss";
 
-Vue.config.productionTip = false
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
-})
+  lintOnSave: false,
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+  pages: {
+    index: {
+      entry: "src/main.js",
+      title: "中国红十字会总会事业发展中心",
+    },
+  },
+});
