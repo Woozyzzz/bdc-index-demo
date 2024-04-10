0eaebe862e26b4361dfdb02afa0d79aa41c9441e
Woozyzzz <woozyzzz@foxmail.com>
2024-04-11 01:58:02 +0800

    chore: build

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === "production" ? "/dist/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
   pages: {
     index: {
       entry: "src/main.js",
